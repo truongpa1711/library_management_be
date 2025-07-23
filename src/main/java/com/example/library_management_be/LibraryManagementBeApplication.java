7b46a7eb7c4c57846518668deea976f987629821
truongpa1711 <truongvunu@gmail.com>
2025-07-23 13:50:42 +0700

    Initial commit: Spring Boot Library Management

---
+++ src/main/java/com/example/library_management_be/LibraryManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_management_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/library_management_be/LibraryManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_management_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
